c1c00dc34f3a215cbb181f4d7e8fb7424f9cb5d6
Pasha <admin@MacBook-Pro-admin.local>
2020-01-06 12:18:10 +0200

    stop bot

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-// Подключаем библиотеку для работы с Telegram API в переменную
+
 var TelegramBot = require('node-telegram-bot-api');
 
-// Устанавливаем токен, который выдавал нам бот
 var token = '907526211:AAHdR8GCDZ6rHXVCTAS-4rX04bOcf3oa4WU';
-// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
-// Подробнее: https://core.telegram.org/bots/api#getupdates
+
+
 var bot = new TelegramBot(token, { polling: true });
 
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+
 bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id; // Получаем ID отправителя
-    var resp = match[1]; // Получаем текст после /echo
+    var fromId = msg.from.id; 
+    var resp = match[1]; 
     bot.sendMessage(fromId, resp);
 });
 
 
 
 const SerhId = "309124685";
-// Простая команда без параметров
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
-    console.log(chatId);
-    console.log(msg);
-    var message = msg.text;
-    bot.sendSticker(chatId, "sticker.webp", {reply_to_message_id:msg.message_id});
 
-    //bot.sendSticker(reply_to_message_id,"sticker.webp")
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id; 
+    //console.log(chatId);
+    //console.log(msg);
+    var message = msg.text;
+    //bot.sendSticker(chatId, "sticker.webp", {reply_to_message_id:msg.message_id});
+    //bot.sendMessage(chatId, "sticker.webp", {reply_to_message_id:msg.message_id});
     var photo = 'sticker.webp'; 
-    //bot.sendPhoto(chatId, photo);
-    if(chatId == SerhId){
-        bot.sendPhoto(chatId, photo);
-    }
+    //if(msg.from.id == SerhId){
+     //   bot.sendSticker(chatId, "sticker.webp", {reply_to_message_id:msg.message_id});
+    //}
 });
 
